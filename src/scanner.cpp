f325dcb24441f43cb1abf9382f9a1cae6a370de9
robbwu <armiuswu@gmail.com>
2025-05-15 01:17:16 -0500

    source/header division for scanner

---
+++ src/scanner.cpp
@@ -0,0 +1,135 @@
+#include "scanner.hpp"
+#include <_ctype.h>
+#include <iostream>
+
+bool Scanner::isAtEnd() {
+    return current >= source.length();
+}
+
+bool Scanner::match(char expected) {
+    if (isAtEnd()) return false;
+    if (source[current] != expected) return false;
+    current++;
+    return true;
+}
+
+char Scanner::peek() {
+    if (isAtEnd()) return '\0';
+    return source[current];
+}
+
+char Scanner::peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source[current+1];
+}
+
+void Scanner::error(int line, std::string_view msg) {
+    std::cout << std::format("line {}: error: {}", line, msg);
+}
+
+void Scanner::string() {
+    while (peek() != '"' && !isAtEnd()) {
+        if (peek() == '\n') line++;
+        advance();
+    }
+
+    if (isAtEnd()) {
+        error(line, "Unterminated string.");
+    }
+
+    advance();
+
+    auto value = source.substr(start+1, current-start-2);
+    addToken(TokenType::STRING, value);
+}
+
+void Scanner::number() {
+    while (isdigit(peek())) advance();
+
+    if (peek() == '.' && isdigit(peekNext())) {
+        advance(); // consume "."
+        while(isdigit(peek())) advance();
+    }
+
+    addToken(TokenType::NUMBER, std::stof(source.substr(start, current-start)));
+}
+
+void Scanner::identifier() {
+    while (isalnum(peek())) advance();
+    auto text = source.substr(start, current-start);
+    auto it = keywords.find(text);
+    auto type = TokenType::IDENTIFIER;
+    // if it's keyword, set toketype to the keyword specific
+    if (it != keywords.end()) {
+        type = it->second;
+    }
+    addToken(type);
+}
+
+void Scanner::scanToken() {
+    char c = advance();
+    switch (c) {
+        case '(': addToken(TokenType::LEFT_PAREN); break;
+        case ')': addToken(TokenType::RIGHT_PAREN); break;
+        case '{': addToken(TokenType::LEFT_BRACE); break;
+        case '}': addToken(TokenType::RIGHT_BRACE); break;
+        case ',': addToken(TokenType::COMMA); break;
+        case '.': addToken(TokenType::DOT); break;
+        case '-': addToken(TokenType::MINUS); break;
+        case '+': addToken(TokenType::PLUS); break;
+        case ';': addToken(TokenType::SEMICOLON); break;
+        case '*': addToken(TokenType::STAR); break;
+        case '!': addToken(match('=') ? TokenType::BANG_EQUAL : TokenType::BANG); break;
+        case '=': addToken(match('=') ? TokenType::EQUAL_EQUAL : TokenType::EQUAL); break;
+        case '<': addToken(match('=') ? TokenType::LESS_EQUAL : TokenType::LESS); break;
+        case '>': addToken(match('=') ? TokenType::GREATER_EQUAL : TokenType::GREATER); break;
+        case '/':
+            if (match('/')) {
+                while (peek() != '\n' && !isAtEnd()) advance();
+            } else {
+                addToken(TokenType::SLASH);
+            }
+            break;
+        case ' ':
+        case '\r':
+        case '\t':
+            break;
+        case '\n':
+            line++;
+            break;
+        // literals
+        case '"': string(); break;
+
+        default:
+            if (isdigit(c)) {
+                number();
+            } else if (isalpha(c)) {
+                identifier();
+            } else {
+                error(line, std::format("Unexpected character: {}", c));
+            }
+            break;
+    }
+}
+
+char Scanner::advance() {
+    return source[current++];
+}
+
+void Scanner::addToken(TokenType type) {
+    addToken(type, nullptr);
+}
+
+void Scanner::addToken(TokenType type, Value literal) {
+    auto text = source.substr(start, current-start);
+    tokens.push_back(new Token(type, text, literal, line));
+}
+
+std::vector<Token*> Scanner::scanTokens() {
+    while (!isAtEnd()) {
+        start = current;
+        scanToken();
+    }
+    tokens.push_back(new Token(TokenType::SCAN_END, "EOF", nullptr, line));
+    return tokens;
+}
--- src/scanner.hpp
+++ src/scanner.hpp
@@ -1,8 +1,10 @@
-#include <_ctype.h>
+#pragma once
+
 #include <string>
 #include <variant>
 #include <format>
-#include <iostream>
+#include <vector>
+#include <unordered_map>
 #include "magic_enum.hpp"
 
 using Value = std::variant<double, std::string, std::nullptr_t>;
@@ -64,8 +66,6 @@ static const std::unordered_map<std::string, TokenType> keywords = {
     {"while",  TokenType::WHILE},
 };
 
-
-
 class Token {
 public:
     TokenType type;
@@ -100,140 +100,20 @@ private:
     std::string source;
     std::vector<Token*> tokens = {};
 
-    bool isAtEnd() {
-        return current >= source.length();
-    }
-
-    bool match(char expected) {
-        if (isAtEnd()) return false;
-        if (source[current] != expected) return false;
-        current++;
-        return true;
-    }
-
-    char peek() {
-        if (isAtEnd()) return '\0';
-        return source[current];
-    }
-
-    char peekNext() {
-        if (current + 1 >= source.length()) return '\0';
-        return source[current+1];
-    }
-
-    void error(int line, std::string_view msg) {
-        std::cout << std::format("line {}: error: {}", line, msg);
-    }
-
-    void string() {
-        while (peek() != '"' && !isAtEnd()) {
-            if (peek() == '\n') line++;
-            advance();
-        }
-
-        if (isAtEnd()) {
-            error(line, "Unterminated string.");
-        }
-
-        advance();
-
-        auto value = source.substr(start+1, current-start-2);
-        addToken(TokenType::STRING, value);
-    }
-
-    void number() {
-        while (isdigit(peek())) advance();
-
-        if (peek() == '.' && isdigit(peekNext())) {
-            advance(); // consume "."
-            while(isdigit(peek())) advance();
-        }
-
-        addToken(TokenType::NUMBER, std::stof(source.substr(start, current-start)));
-    }
-
-    void identifier() {
-        while (isalnum(peek())) advance();
-        auto text = source.substr(start, current-start);
-        auto it = keywords.find(text);
-        auto type = TokenType::IDENTIFIER;
-        // if it's keyword, set toketype to the keyword specific
-        if (it == keywords.end()) {
-            type = it->second;
-        }
-        addToken(type);
-    }
-
-    void scanToken() {
-        char c  = advance();
-        switch (c) {
-            case '(': addToken(TokenType::LEFT_PAREN); break;
-            case ')': addToken(TokenType::RIGHT_PAREN); break;
-            case '{': addToken(TokenType::LEFT_BRACE); break;
-            case '}': addToken(TokenType::RIGHT_BRACE); break;
-            case ',': addToken(TokenType::COMMA); break;
-            case '.': addToken(TokenType::DOT); break;
-            case '-': addToken(TokenType::MINUS); break;
-            case '+': addToken(TokenType::PLUS); break;
-            case ';': addToken(TokenType::SEMICOLON); break;
-            case '*': addToken(TokenType::STAR); break;
-            case '!': addToken(match('=') ? TokenType::BANG_EQUAL : TokenType::BANG); break;
-            case '=': addToken(match('=') ? TokenType::EQUAL_EQUAL : TokenType::EQUAL); break;
-            case '<': addToken(match('=') ? TokenType::LESS_EQUAL : TokenType::LESS); break;
-            case '>': addToken(match('=') ? TokenType::GREATER_EQUAL : TokenType::GREATER); break;
-            case '/':
-                if (match('/')) {
-                    while (peek() != '\n' && !isAtEnd()) advance();
-                } else {
-                    addToken(TokenType::SLASH);
-                }
-                break;
-            case ' ':
-            case '\r':
-            case '\t':
-                break;
-            case '\n':
-                line++;
-                break;
-            // literals
-            case '"': string(); break;
-
-
-            default:
-                if (isdigit(c)) {
-                    number();
-                } else if (isalpha(c)) {
-                    identifier();
-                } else {
-                    error(line, std::format("Unexpected character: {}", c));
-                }
-                break;
-        }
-    }
-
-    char advance() {
-        return source[current++];
-    }
-
-    void addToken(TokenType type) {
-        addToken(type, nullptr);
-    }
-
-    void addToken(TokenType type, Value literal) {
-        auto text = source.substr(start, current-start);
-        tokens.push_back(new Token(type, text, literal, line));
-    }
+    bool isAtEnd();
+    bool match(char expected);
+    char peek();
+    char peekNext();
+    void error(int line, std::string_view msg);
+    void string();
+    void number();
+    void identifier();
+    void scanToken();
+    char advance();
+    void addToken(TokenType type);
+    void addToken(TokenType type, Value literal);
 
 public:
     Scanner(std::string _source): source(_source) {}
-
-    std::vector<Token*> scanTokens() {
-        while (!isAtEnd()) {
-            start = current;
-            scanToken();
-        }
-        tokens.push_back(new Token(TokenType::SCAN_END, "EOF", nullptr, line));
-        return tokens;
-    }
-
+    std::vector<Token*> scanTokens();
 };
